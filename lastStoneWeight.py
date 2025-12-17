eae102381075598a8fc1ee2beb6fdc1fdae3ff8b
Pragati Gupta <praeast94@gmail.com>
2025-12-17 15:54:26 +0530

    lastStoneWeight problem solved using heapify

--- lastStoneWeight.py
+++ lastStoneWeight.py
@@ -1,7 +1,8 @@
+import heapq
 from typing import List
 
 class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
+    def lastStoneWeightUsingSort(self, stones: List[int]) -> int:
         while len(stones) > 1:
             y = max(stones)
             stones.remove(y)
@@ -14,6 +15,19 @@ class Solution:
             return 0 
         return stones[0]
     
+    def lastStoneWeight(self, stones: list[int])-> int:
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            x = heapq.heappop(stones)
+            y = heapq.heappop(stones)
+            if y > x:
+                heapq.heappush(stones, y-x)
+        if len(stones) == 0:
+            return 0
+        return abs(stones[0])
+
+
 result = Solution()
 stones = [1,2]
 print(result.lastStoneWeight(stones))
