b0496f4880d32fd684ae852c5c3620faa9c37a17
Pragati Gupta <praeast94@gmail.com>
2025-12-18 11:22:29 +0530

    solved question to find median in a tree using heap and priority queue

---
+++ medianFinder.py
@@ -0,0 +1,12 @@
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        
+
+    def findMedian(self) -> float:
