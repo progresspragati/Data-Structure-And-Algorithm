602c3aba1202794ede3309d87ed285acf60e436a
Pragati Gupta <praeast94@gmail.com>
2026-01-30 15:40:47 +0530

    modified longestSubssequence.py with unnecessary lines removal

--- longestSubsequence.py
+++ longestSubsequence.py
@@ -2,13 +2,12 @@ from typing import List
 
 
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLIS1(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
         dp = [1]*len(nums)
-        dp[0] = 1
-        
         i = 0
         j = i+1
-        max_length = 1
         while j < len(nums):
             if i == j:
                 i = 0
@@ -17,9 +16,17 @@ class Solution:
             elif nums[i] < nums[j]:
                 dp[j] = max(dp[j], dp[i]+1)
             i += 1
-        for i in range(len(dp)):
-            max_length = max(max_length, dp[i])
-        return max_length
+        return max(dp)
+    
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        dp = [1]*len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        return max(dp)
     
 result = Solution()
 print(result.lengthOfLIS([0,3,1,3,2,3]))
