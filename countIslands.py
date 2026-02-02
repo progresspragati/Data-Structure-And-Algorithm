21c202f0f5e42d364ca91b8ced0cb261e85bcb2e
Pragati Gupta <praeast94@gmail.com>
2026-02-02 11:30:01 +0530

    repracticed countIslands

--- countIslands.py
+++ countIslands.py
@@ -52,7 +52,49 @@ class Solution:
                     bfs(i, j)
                     islands_count += 1
         return islands_count
+    
+    def numIslandsUsingDFS(self, grid: List[List[str]]) -> int:
+        def DFS(grid, i, j):
+            if  len(grid) <= i or i < 0 or len(grid[0]) <= j  or j < 0 or grid[i][j] == "0":
+                return 
+            grid[i][j] = "0"
+            DFS(grid, i+1, j)
+            DFS(grid, i-1, j)
+            DFS(grid, i, j+1)
+            DFS(grid, i, j-1)
+        
+        num_of_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    DFS(grid, i, j)
+                    num_of_islands += 1
+        return num_of_islands
+    
+    def numIslandsUsingBFS(self, grid: List[List[str]]) -> int:
+        def bfs(i, j):
+            q = deque()
+            q.append((i, j))
+            grid[i][j] = "0"
+            while q:
+                x, y = q.popleft()
+                for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    nx, ny = dx+x, dy+y
+                    if (0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == "1"): 
+                        grid[nx][ny] = "0"
+                        q.append((nx, ny))
+
+        num_of_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] != "0":
+                    bfs(i, j)
+                    num_of_islands += 1
+        return num_of_islands
+
+
+
      
     
 result = Solution()
-print(result.numIslandsUsingBfs([["1","1","0","0","1"],["1","1","0","0","1"],["0","0","1","0","0"],["0","0","0","1","1"]]))
+print(result.numIslandsUsingBFS([["1","1","0","0","1"],["1","1","0","0","1"],["0","0","1","0","0"],["0","0","0","1","1"]]))
