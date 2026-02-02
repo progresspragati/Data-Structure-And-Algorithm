c33d0c4b90f2cd1e0074a29cd14507c53ddd7bca
Pragati Gupta <praeast94@gmail.com>
2026-02-02 12:55:53 +0530

    find out indices from where water can reach to atlantic and pacific ocean using bfs

--- pacificAtlantic.py
+++ pacificAtlantic.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 
 
@@ -31,8 +32,47 @@ class Solution:
                     res.append([r,c])
         return res
     
+    def pacificAtlanticUsingBfs(self, heights: List[List[int]]) -> List[List[int]]:
+        rows = len(heights)
+        cols = len(heights[0])
+        directions = {(1,0), (-1,0), (0,1), (0,-1)}
+        pac = [[False] * cols for _ in range(rows)] 
+        atl = [[False] * cols for _ in range(rows)] 
+
+        def bfs(source, ocean):
+            q = deque(source)
+            while q:
+                r, c = q.popleft()
+                ocean[r][c] = True
+                for dr, dc in directions:
+                    nr, nc = dr+r, dc+c
+                    if nr >=0 and nr < rows and nc >= 0 and nc < cols and not ocean[nr][nc] and heights[nr][nc] >= heights[r][c]:
+                        q.append((nr,nc))
+
+        pacific = []
+        atlantic = []
+        for c in range(cols):
+            pacific.append((0,c))
+            atlantic.append((rows-1,c))
+        
+        for r in range(rows):
+            pacific.append((r,0))
+            atlantic.append((r,cols-1))
+        
+        bfs(pacific, pac)
+        bfs(atlantic, atl)
+        res = []
+
+        for r in range(rows):
+            for c in range(cols):
+                if pac[r][c] and atl[r][c]:
+                    res.append([r,c])
+        return res
+
+
+    
 result = Solution()
-print(result.pacificAtlantic([
+print(result.pacificAtlanticUsingBfs([
   [4,2,7,3,4],
   [7,4,6,4,7],
   [6,3,5,3,6]
